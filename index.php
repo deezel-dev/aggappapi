7e2ec18647dda443c2f5f66a4e4b00f9694aa4bc
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-07 01:53:12 -0400

    f

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
-   require_once $_SERVER['DOCUMENT_ROOT'] . "/include/_init.php";
-   $ROOT = $_SERVER['DOCUMENT_ROOT'];
+   require_once $_SERVER['HTTP_HOST'] . "/include/_init.php";
+   $ROOT = $_SERVER['HTTP_HOST'];
 
    $profileID = -1;
    $isUser = FALSE;
-   $rootPath = $_SERVER['DOCUMENT_ROOT'];
+   $rootPath = $_SERVER['HTTP_HOST'];
    
    if (isset($_SESSION[SESSION_PROFILE_ID])) {
        $profileID = $_SESSION[SESSION_PROFILE_ID];
@@ -16,12 +16,22 @@
 <!DOCTYPE html>
 <html data-ng-app="app" lang="en" style="width: 100%;height: 100%;margin: 0px;padding: 0px;overflow-x: hidden;">
    <head>
-      <?php require $_SERVER['DOCUMENT_ROOT'] . "/include/layout/main.head.php" ?>
+      <?php require $_SERVER['HTTP_HOST'] . "/include/layout/main.head.php" ?>
    </head>
     <body data-ng-controller="indexCtrl" data-ng-init="setProfileData(<?php echo($profileID) ?>)">        
        
-       <div><font color="#000">AGG APP API   <?php echo $_SERVER['DOCUMENT_ROOT'] ?> </font></div>
-        
+       <div><font color="#000">AGG APP API   <?php echo $_SERVER['HTTP_HOST'] ?> </font></div>
+        <!-- content ui-view -->
+        <div style ="margin-top: 40px;" ui-view></div> <!--  autoscroll="true"  -->
+
+        <!-- static navbar - top -->
+        <div style="position: fixed; top: 0px;width: 100%;margin: 0px;padding: 0px;">
+        <?php require $_SERVER['HTTP_HOST'] . "/include/layout/main.body-navbar.php" ?>
+        </div>
+
+        <!-- load all scripts -->
+        <?php require $_SERVER['HTTP_HOST'] . "/include/layout/main.body-scripts.php" ?>
+        <script src="/flix/app.js"></script>
 
     </body>
 </html>
