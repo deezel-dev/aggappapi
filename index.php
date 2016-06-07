2f71e440e0f2ca988a4f495f5b2436de3432e4c3
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-07 02:19:18 -0400

    a

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
    $ROOT = "http://" . $_SERVER['HTTP_HOST'];
-   require_once $ROOT . "/include/_init.php";
+   //require_once $ROOT . "/include/_init.php";
    echo $ROOT . "/include/_init.php";
 ?>
 
