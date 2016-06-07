72bafcc51f040398d88257b343675d061f6f6e72
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-07 02:16:43 -0400

    s

--- index.php
+++ index.php
@@ -1,21 +1,9 @@
 <?php
-
-
    $ROOT = "http://" . $_SERVER['HTTP_HOST'];
-
    require_once $ROOT . "/include/_init.php";
+   echo $ROOT . "/include/_init.php";
+?>
 
-   $profileID = -1;
-   $isUser = FALSE;
-   $rootPath = $ROOT;
-   
-   if (isset($_SESSION[SESSION_PROFILE_ID])) {
-       $profileID = $_SESSION[SESSION_PROFILE_ID];
-       $isUser = TRUE;
-   }
-
-   
-   ?>
 <!DOCTYPE html>
 <html data-ng-app="app" lang="en" style="width: 100%;height: 100%;margin: 0px;padding: 0px;overflow-x: hidden;">
    <head>
