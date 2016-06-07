1702db866428f6373e31d8b42c1bd31f89089661
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-07 01:44:03 -0400

    d

--- include/layout/partial/script-ga.php
+++ include/layout/partial/script-ga.php
@@ -1,4 +1,4 @@
-<?php if ($GLOBALS["IsDevelopment"] == 0) { ?>
+<?php if ($GLOBALS["IsDevelopment"] == 999) { ?>
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
