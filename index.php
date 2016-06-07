6b0cebea9bcfd9cbf7cec88ae0e3a99470d9ad24
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-07 02:18:22 -0400

    d

--- index.php
+++ index.php
@@ -4,25 +4,4 @@
    echo $ROOT . "/include/_init.php";
 ?>
 
-<!DOCTYPE html>
-<html data-ng-app="app" lang="en" style="width: 100%;height: 100%;margin: 0px;padding: 0px;overflow-x: hidden;">
-   <head>
-      <?php require $ROOT . "/include/layout/main.head.php" ?>
-   </head>
-    <body data-ng-controller="indexCtrl" data-ng-init="setProfileData(<?php echo($profileID) ?>)">        
-       
-       <div><font color="#000"></font>AGG APP API   <?php echo $ROOT ?></div>
-        <!-- content ui-view -->
-        <div style ="margin-top: 40px;" ui-view></div> <!--  autoscroll="true"  -->
 
-        <!-- static navbar - top -->
-        <div style="position: fixed; top: 0px;width: 100%;margin: 0px;padding: 0px;">
-        <?php require $ROOT . "/include/layout/main.body-navbar.php" ?>
-        </div>
-
-        <!-- load all scripts -->
-        <?php require $ROOT . "/include/layout/main.body-scripts.php" ?>
-        <script src="/flix/app.js"></script>
-
-    </body>
-</html>
