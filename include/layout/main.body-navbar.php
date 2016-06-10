21008a487a6a71889ab298318ddcd24c4c8aefc1
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 01:23:09 -0400

    sd

--- include/layout/main.body-navbar.php
+++ include/layout/main.body-navbar.php
@@ -2,7 +2,7 @@
     //$ROOT = "http://" . $_SERVER['HTTP_HOST']; 
 ?>
 
-<header class="navbar navbar-default navbar-static-top" style="background:#0f7">
+<header class="navbar navbar-default navbar-static-top" style="background-color: #CC3333">
    
    <div class="container-fluid">
       <div class="navbar-header">
