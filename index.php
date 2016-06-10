8e4981fb8267099e3d56fc4a3a589def756d6a6c
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-09 23:53:20 -0400

    s

--- index.php
+++ index.php
@@ -6,20 +6,20 @@
 <!DOCTYPE html>
 <html data-ng-app="app" lang="en" style="width: 100%;height: 100%;margin: 0px;padding: 0px;overflow-x: hidden;">
    <head>
-      <?php require "http://aggappapi.azurewebsites.net/include/layout/main.head.php" ?>
+      <?php //require "http://aggappapi.azurewebsites.net/include/layout/main.head.php" ?>
    </head>
     <body data-ng-controller="indexCtrl">        
-       
+        <div>INDEX</div>
         <!-- content ui-view -->
         <div style ="margin-top: 40px;" ui-view></div> <!--  autoscroll="true"  -->
 
         <!-- static navbar - top  -->
         <div style="position: fixed; top: 0px;width: 100%;margin: 0px;padding: 0px;">
-        <?php require "http://aggappapi.azurewebsites.net/include/layout/main.body-navbar.php" ?>
+        <?php //require "http://aggappapi.azurewebsites.net/include/layout/main.body-navbar.php" ?>
         </div>
 
         <!-- load all scripts -->
-        <?php require "http://aggappapi.azurewebsites.net/include/layout/main.body-scripts.php" ?>
+        <?php //require "http://aggappapi.azurewebsites.net/include/layout/main.body-scripts.php" ?>
         <script src="http://aggappapi.azurewebsites.net/flix/app.js"></script>
 
     </body>
