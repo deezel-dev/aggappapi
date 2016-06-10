2276191957ee4b308df46cc1ef327a53dc6ff1f8
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 00:26:40 -0400

    s

--- index.php
+++ index.php
@@ -1,40 +1,27 @@
 <?php
    $ROOT = "http://" . $_SERVER['HTTP_HOST'];
-   $INIT = $_SERVER['HTTP_HOST'] . "/include/_init.php";
-   echo $INIT;
    require_once ("/include/_init.php");
-   echo testInit();
-   //echo $ROOT . "<br>rtertwertwret<br>";
    
 ?>
 <!DOCTYPE html>
 <html data-ng-app="app" lang="en" style="width: 100%;height: 100%;margin: 0px;padding: 0px;overflow-x: hidden;">
-    <div>INDEX</div>
-</html>
 
-<!-- 
-    //<?php
-   //$ROOT = "http://" . $_SERVER['HTTP_HOST'];
-   //require_once //"http://aggappapi.azurewebsites.net/include/_init.php";
-//?>
        <head>
-      <?php //require "http://aggappapi.azurewebsites.net/include/layout/main.head.php" ?>
+      <?php require ("/include/layout/main.head.php") ?>
    </head>
     <body data-ng-controller="indexCtrl">        
         <div>INDEX</div>
-        <!-- content ui-view 
+        <!-- content ui-view -->
         <div style ="margin-top: 40px;" ui-view></div> <!--  autoscroll="true"  -->
 
-        <!-- static navbar - top  
+        <!-- static navbar - top -->
         <div style="position: fixed; top: 0px;width: 100%;margin: 0px;padding: 0px;">
-        <?php //require "http://aggappapi.azurewebsites.net/include/layout/main.body-navbar.php" ?>
+        <?php require ("/include/layout/main.body-navbar.php") ?>
         </div>
 
-        <!-- load all scripts 
-        <?php //require "http://aggappapi.azurewebsites.net/include/layout/main.body-scripts.php" ?>
-        <script src="http://aggappapi.azurewebsites.net/flix/app.js"></script>
+        <!-- load all scripts -->
+        <?php require ("/include/layout/main.body-scripts.php") ?>
+        <script src="/flix/app.js"></script>
 
     </body>
-    
-    -->
-
+</html>
