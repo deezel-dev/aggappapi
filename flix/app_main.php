baceb19dd22e622d126cf26c98a1d3e5e6c5dfd0
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 02:38:05 -0400

    sdsd

--- flix/app_main.php
+++ flix/app_main.php
@@ -11,7 +11,7 @@
                 <u><b><h3>Farmers Market Directory API</h3></b></u>
                 <input type="text" PLACEHOLDER='Enter Zip Code' ng-model="zip_code"/>
                 <button ng-click="btnSearchZip(zip_code)" class="btn btn-primary btn-xs">Search</button>
-                <br>
+                <br><br>
                 <div id="market_list" ng-repeat="market in markets">
                     <font color="#000">{{market.id}}: &nbsp;&nbsp; {{market.marketname}} </font> 
                 </div>
