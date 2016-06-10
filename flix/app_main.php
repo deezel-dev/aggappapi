8546666b5d559fedb52f628564d4982e5ff269d6
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 01:07:52 -0400

    dlkj

--- flix/app_main.php
+++ flix/app_main.php
@@ -10,8 +10,5 @@
         <!-- home -->
         <div id="home"><?php require ("/flix/landing_page/01_home.html") ?></div>
 
-        <!-- contact -->
-        <div id="contact"><?php require ("/flix/flix_main_contact_form.html") ?></div>
-
    </body>
 </html>
