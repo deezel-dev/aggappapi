8ba9e17309b0feada08e8e5f78bbbc656a05bee9
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 00:50:31 -0400

    s

--- flix/app_main.php
+++ flix/app_main.php
@@ -10,24 +10,6 @@
         <!-- home -->
         <div id="home"><?php require ("/flix/landing_page/01_home.html") ?></div>
 
-        <!-- how  -->
-        <div id="how"><?php require ("/flix/landing_page/02_how.html") ?></div>
-
-        <div class="row">
-            <!-- about  -->
-            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                <div id="about"><?php require ("/flix/landing_page/03_about.html") ?></div>
-            </div>
-            <!-- news  -->
-            <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
-                <div id="updates"><?php require ("/flix/landing_page/04_updates.html") ?></div>
-            </div>
-        </div>
-
-        <!-- screenshots -->
-        <div id="screenshots"><?php require ("/flix/landing_page/05_screenshots.html") ?></div>
-            
-
         <!-- contact -->
         <div id="contact"><?php require ("/flix/flix_main_contact_form.html") ?></div>
 
