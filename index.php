27c07210cdaf0bf24c5abd08812055aed0424ed2
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-09 23:59:39 -0400

    s

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
    $ROOT = "http://" . $_SERVER['HTTP_HOST'];
    echo $ROOT . "<br><br>";
-   //require_once //"http://aggappapi.azurewebsites.net/include/_init.php";
+   require_once $ROOT . "/include/_init.php";
 ?>
 <!DOCTYPE html>
 <html data-ng-app="app" lang="en" style="width: 100%;height: 100%;margin: 0px;padding: 0px;overflow-x: hidden;">
