a4c97519454fcf947a8904899cc86016b3a4ec9c
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 00:12:59 -0400

    s

--- include/_init.php
+++ include/_init.php
@@ -1,5 +1,18 @@
 <?php
-echo "<b><u>INCLUDE1</u></b>";
+
+$GLOBALS["IsDevelopment"] = 1;
+
+function testInit(){
+    return "<b><u>INIT</u></b>";
+}
+
+function displayErrorIfDevelopment() {
+    if ($GLOBALS["IsDevelopment"] == 1) {
+        ?>
+        window.alert("ERROR:\n\n" + data);
+        <?php
+    }
+}
 
 /*
 $GLOBALS["IsDevelopment"] = 1; //($_SERVER['HTTP_HOST'] == "aggappapi.azurewebsites.net") || ($_SERVER['HTTP_HOST'] == "flixacademy-dev-danny.azurewebsites.net") ? 1 : 0;
@@ -12,13 +25,7 @@ if ($GLOBALS["IsDevelopment"] == 1) {
     error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED);
 }
 
-function displayErrorIfDevelopment() {
-    if ($GLOBALS["IsDevelopment"] == 1) {
-        ?>
-        window.alert("ERROR:\n\n" + data);
-        <?php
-    }
-}
+
 
 require_once "http://aggappapi.azurewebsites.net/include/constants.php";
 session_start();
