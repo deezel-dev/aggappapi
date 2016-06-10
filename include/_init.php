4174eaf84b6847b9c4319fd1b0f5da62d4ab91b1
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-10 00:21:58 -0400

    s

--- include/_init.php
+++ include/_init.php
@@ -1,34 +1,22 @@
 <?php
 
-$GLOBALS["IsDevelopment"] = 1;
+    $GLOBALS["IsDevelopment"] = 1;
 
-function testInit(){
-    return "<b><u>INIT</u></b>";
-}
-
-function displayErrorIfDevelopment() {
     if ($GLOBALS["IsDevelopment"] == 1) {
-        ?>
-        window.alert("ERROR:\n\n" + data);
-        <?php
+        ini_set('display_errors',1);
+        ini_set('display_startup_errors',1);
+        error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED);
     }
-}
 
-/*
-$GLOBALS["IsDevelopment"] = 1; //($_SERVER['HTTP_HOST'] == "aggappapi.azurewebsites.net") || ($_SERVER['HTTP_HOST'] == "flixacademy-dev-danny.azurewebsites.net") ? 1 : 0;
+    function displayErrorIfDevelopment() {
+        if ($GLOBALS["IsDevelopment"] == 1) {
+            ?>
+            window.alert("ERROR:\n\n" + data);
+            <?php
+        }
+    }
 
-echo "<b><u>INCLUDE2</u></b>";
+    require_once ("/include/constants.php");
+    session_start();
 
-if ($GLOBALS["IsDevelopment"] == 1) {
-    ini_set('display_errors',1);
-    ini_set('display_startup_errors',1);
-    error_reporting(E_ALL & ~E_WARNING & ~E_DEPRECATED);
-}
-
-
-
-require_once "http://aggappapi.azurewebsites.net/include/constants.php";
-session_start();
-echo "<b><u>INCLUDE</u></b>";
-*/
 ?>
