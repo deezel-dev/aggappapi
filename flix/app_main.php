42fe33687de6f4b7b2202f7a1a2d5d75765e19ff
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-11 02:58:33 -0400

    s

--- flix/app_main.php
+++ flix/app_main.php
@@ -12,7 +12,9 @@
                     <!-- <h2>Farmers Market</h2> -->
                     <img src="/public/images/farmers_market.jpg" alt="Farmers Market" style="width:100%;height:100%;">
                </div>
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12" style="margin-top:20px;">
+                    <!-- <h2>Farmers Market</h2> -->
+                    <img src="/public/images/weather.jpg" alt="Weather" style="width:100%;height:100%;">
                </div>
             </div>
             
