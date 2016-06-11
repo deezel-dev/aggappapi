aaad32eeeffd3ac3efc64b79f810a27e134bbea1
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-11 02:53:34 -0400

    s

--- flix/app_main.php
+++ flix/app_main.php
@@ -8,9 +8,9 @@
         <div id="home" class="container" align="center">
             
             <div class="row">
-                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6" style="padding:8px;">
                     <!-- <h2>Farmers Market</h2> -->
-                    <img src="/public/images/farmers_market.jpg" alt="Farmers Market" style="width:75%;height:85%;">
+                    <img src="/public/images/farmers_market.jpg" alt="Farmers Market" style="width:100%;height:100%;">
                </div>
                 <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
                </div>
