30928aa48e720110eb9b8bd3c3e940ab8f57b469
Daniel Sostre <Deezel@Daniels-MacBook-Air.local>
2016-06-11 00:16:12 -0400

    a

--- flix/app_main.php
+++ flix/app_main.php
@@ -8,9 +8,12 @@
         <div id="home" class="container">
             
             <div class="row">
-                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <u><b><h3>Farmers Market Directory</h3></b></u>
                </div>
+            </div>
+            
+            <div class="row">
                 <div class="col-lg-3 col-md-4 col-sm-9 col-xs-9" style="display:block;">
                     <input type="text" PLACEHOLDER='Enter Zip Code' ng-model="zip_code"/>
                 </div>
